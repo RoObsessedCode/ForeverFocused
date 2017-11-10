404c394a88f230bf450ed65ddbaa46127e4d1a8b
Rohan Saigal <rohan4saigal@gmail.com>
2017-11-10 12:55:31 -0500

    have to priority views rendering beautifully

--- screens/PrioritiesScreen.js
+++ screens/PrioritiesScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, ScrollView, StyleSheet, Text, TextInput} from 'react-native';
+import { View, ScrollView, StyleSheet, Text, TextInput } from 'react-native';
 import TimePicker from 'react-native-simple-time-picker'
 import { ExpoLinksView } from '@expo/samples';
 
@@ -11,8 +11,12 @@ export default class PrioritiesScreen extends Component {
       fieldA: '',
       fieldB: '',
       fieldC: '',
-      selectedHours: 0,
-      selectedMinutes: 0,
+      selectedHoursA: 0,
+      selectedMinutesA: 0,
+      selectedHoursB: 0,
+      selectedMinutesB: 0,
+      selectedHoursC: 0,
+      selectedMinutesC: 0,
 
     }
     this.onChangeText = this.onChangeText.bind(this)
@@ -33,39 +37,59 @@ export default class PrioritiesScreen extends Component {
     const numTextFieldInputs = 3
     let textFields = ["fieldA", "fieldB", "fieldC"]
     const passion = "Today I will get closer to my DREAMS by..."
-    const { selectedHours, selectedMinutes} = this.state
+    const { selectedHoursA, selectedMinutesA, selectedHoursB, selectedMinutesB, selectedHoursC, selectedMinutesC } = this.state
 
     return (
       <ScrollView>
         <View style={styles.container}>
           <Text style={styles.prioritiesTitle}>PRIORITIES</Text>
-          <TextInput style={styles.textField}
-            placeholder={passion}
-            value={this.state.fieldA}
-            onChangeText={(value) => this.onChangeText(value, 'fieldA')}
-          />
-          <View style={styles.timeContainer}>
 
-            <TimePicker
-              selectedHours = {selectedHours}
-              selectedMinutes = {selectedMinutes}
-              onChange={(hours, minutes) => this.setState({ selectedHours: hours, selectedMinutes: minutes})}
+          <View style={styles.priContainer}>
+            <Text style={styles.timeDisplay}>{selectedHoursA}:{selectedMinutesA}</Text>
+            <TextInput style={styles.textField}
+              placeholder={passion}
+              value={this.state.fieldA}
+              onChangeText={(value) => this.onChangeText(value, 'fieldA')}
             />
-            <Text style={styles.timeDisplay}>{selectedHours}:{selectedMinutes}</Text>
+
+            <TimePicker style={styles.timeScroller}
+              selectedHours={selectedHoursA}
+              selectedMinutes={selectedMinutesA}
+              onChange={(hours, minutes) => this.setState({ selectedHoursA: hours, selectedMinutesA: minutes })}
+            />
 
           </View>
 
-          <TextInput style={styles.textField}
-            placeholder={passion}
-            value={this.state.fieldB}
-            onChangeText={(value) => this.onChangeText(value, 'fieldB')}
-          />
+          <View style={styles.priContainer}>
+            <Text style={styles.timeDisplay}>{selectedHoursB}:{selectedMinutesB}</Text>
+            <TextInput style={styles.textField}
+              placeholder={passion}
+              value={this.state.fieldB}
+              onChangeText={(value) => this.onChangeText(value, 'fieldB')}
+            />
 
-          <TextInput style={styles.textField}
-            placeholder={passion}
-            value={this.state.fieldC}
-            onChangeText={(value) => this.onChangeText(value, 'fieldC')}
-          />
+            <TimePicker style={styles.timeScroller}
+              selectedHours={selectedHoursB}
+              selectedMinutes={selectedMinutesB}
+              onChange={(hours, minutes) => this.setState({ selectedHoursB: hours, selectedMinutesB: minutes })}
+            />
+
+          </View>
+          {/* <View style={styles.priContainer}>
+            <Text style={styles.timeDisplay}>{selectedHoursC}:{selectedMinutesC}</Text>
+            <TextInput style={styles.textField}
+              placeholder={passion}
+              value={this.state.fieldC}
+              onChangeText={(value) => this.onChangeText(value, 'fieldC')}
+            />
+
+            <TimePicker style={styles.timeScroller}
+              selectedHours={selectedHoursC}
+              selectedMinutes={selectedMinutesC}
+              onChange={(hours, minutes) => this.setState({ selectedHoursC: hours, selectedMinutesC: minutes })}
+            />
+
+          </View> */}
 
         </View>
       </ScrollView>
@@ -81,33 +105,41 @@ const styles = StyleSheet.create({
     //resizeMode: 'contain',
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     paddingTop: 15,
     backgroundColor: '#fff',
     height: 600,
 
   },
+  priContainer: {
+    display: 'flex',
+    height: 290
+  },
   prioritiesTitle: {
     fontSize: 32,
     color: '#570674',
-    textAlign: 'center'
+    textAlign: 'center',
+    marginTop: 15,
+    marginBottom: 15
   },
   textField: {
-    flex: 1,
-    margin: 10,
+    //flex: 1,
+    marginLeft: 25,
+    marginRight: 25,
+    padding: 5,
     backgroundColor: '#1e90ff',
     borderRadius: 25,
-    margin: 40
+
   },
-  timeContainer: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  timeScroller: {
   },
+  // timeContainer: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
   timeDisplay: {
-    // flex: 1,
-    // height: 25,
-    // padding: 5,
+    textAlign: 'center'
   },
 })
